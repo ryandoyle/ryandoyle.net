199e26ae80be0cc87d5cda485b4b561a3ee48da5
Ryan Doyle <ryan@doylenet.net>
2023-05-30 19:49:49 +1000

    Remove www redirect

--- terraform/site.tf
+++ terraform/site.tf
@@ -104,20 +104,6 @@ resource "aws_cloudfront_distribution" "site" {
   }
 }
 
-resource "aws_s3_bucket" "redirect" {
-  bucket = "www.ryandoyle.net"
-}
-resource "aws_s3_bucket_acl" "redirect" {
-  bucket = aws_s3_bucket.redirect.id
-  acl = "public-read"
-}
-resource "aws_s3_bucket_website_configuration" "redirect" {
-  bucket = aws_s3_bucket.redirect.id
-  redirect_all_requests_to {
-    host_name = "ryandoyle.net"
-  }
-}
-
 output "s3_bucket" {
   value = aws_s3_bucket.site.bucket
 }
@@ -133,17 +119,5 @@ resource "aws_route53_record" "apex" {
     name = aws_cloudfront_distribution.site.domain_name
 #    zone_id = aws_s3_bucket.site.hosted_zone_id
     zone_id = aws_cloudfront_distribution.site.hosted_zone_id
-  }
-}
-
-resource "aws_route53_record" "www" {
-  name = "www.ryandoyle.net"
-  type = "A"
-  zone_id = "Z2KH05FAS8W0B5"
-
-  alias {
-    evaluate_target_health = false
-    name = aws_s3_bucket_website_configuration.redirect.website_domain
-    zone_id = aws_s3_bucket.redirect.hosted_zone_id
   }
 }
